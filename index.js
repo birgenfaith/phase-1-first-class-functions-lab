7782d2c6d606dc3edcb6014723bde924bb419c38
Faith Chepkoech Birgen <faybirgen815@gmail.com>
2024-12-06 15:29:41 +0300

    add solutions on index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const theDrivers = (['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+const returnFirstTwoDrivers = function(){
+       return theDrivers.slice(0,2);
+
+}
+returnFirstTwoDrivers();
+
+const returnLastTwoDrivers = function(){
+        return theDrivers.slice(2,4);
+}
+returnLastTwoDrivers();
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (num1) => (integer) =>{
+        return num1 * integer;
+}
+
+const fareDoubler =createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(theDrivers, selectDrivers){
+        return selectDrivers(theDrivers)
+};
